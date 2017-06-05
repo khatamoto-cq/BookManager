640bde151b811736ccce3b60d1d2648704bfe3bf
hatamoto kanji <hatamoto@hatamotonoMacBook-Pro.local>
2017-06-05 11:53:14 +0900

    Initial Commit

---
+++ BookManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookManager
+//
+//  Created by hatamoto kanji on 2017/06/05.
+//  Copyright © 2017年 hatamoto kanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
