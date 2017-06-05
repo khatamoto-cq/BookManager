82d7ab1271e02a1b713a6f12ae9d5b7999b49543
hatamoto <k.hatamoto@caraquri.com>
2017-06-05 12:36:47 +0900

    ログイン画面のテキストフィールドとログインボタンを調整
    ・テキストフィールドが見えにくいので枠をつけた
    ・テキストフィールドとボタンを角丸にした

--- BookManager/ViewController.swift
+++ BookManager/ViewController.swift
@@ -12,10 +12,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // メール(テキストフィールド)
+        mailField.layer.borderWidth = 1
+        mailField.layer.cornerRadius = 5
+        mailField.layer.borderColor = UIColor.lightGray.cgColor
+        mailField.layer.masksToBounds = true
+        
+        // パスワード(テキストフィールド)
+        passwordField.layer.borderWidth = 1
+        passwordField.layer.cornerRadius = 5
+        passwordField.layer.borderColor = UIColor.lightGray.cgColor
+        passwordField.layer.masksToBounds = true
+        
+        // ログイン(ボタン)
+        loginButton.layer.cornerRadius = 5
     }
 }
 
