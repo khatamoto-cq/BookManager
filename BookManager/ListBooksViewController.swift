cf92ccbaf360acb8baccd112ac362a5ccff142eb
hatamoto <k.hatamoto@caraquri.com>
2017-06-06 12:58:01 +0900

    書籍一覧をViewControllerからTableViewControllerに変更

--- BookManager/ListBooksViewController.swift
+++ BookManager/ListBooksViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ListBooksViewController: UIViewController {
+class ListBooksViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
