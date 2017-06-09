42859daf00fb49d688e0894c7a534993d08f9556
hatamoto <k.hatamoto@caraquri.com>
2017-06-09 19:34:42 +0900

    モデルのプロパティを正しく修正

--- BookManager/Book.swift
+++ BookManager/Book.swift
@@ -5,18 +5,18 @@ struct Book {
     // MARK: Properties
     let name: String
     let price: String
-    let publishDate: String
+    let purchaseDate: String
     let imageUrl: String
 
     static let Name = "name"
     static let Price = "price"
-    static let PublishDate = "publishDate"
+    static let PurchaseDate = "purchaseDate"
     static let ImageUrl = "imageUrl"
 
     init(dictionay: [String : String]) {
         self.name = dictionay[Book.Name]!
         self.price = dictionay[Book.Price]!
-        self.publishDate = dictionay[Book.PublishDate]!
+        self.purchaseDate = dictionay[Book.PurchaseDate]!
         self.imageUrl = dictionay[Book.ImageUrl]!
     }
 }
@@ -33,11 +33,11 @@ extension Book {
 
     static func load() -> [[String : String]] {
         return [
-            [Book.Name: "パーフェクトPHP", Book.Price: "3000", Book.PublishDate: "2013/04/13", Book.ImageUrl: "image1.png"],
-            [Book.Name: "CakePHP辞典", Book.Price: "3000", Book.PublishDate: "2013/04/13", Book.ImageUrl: "image2.png"],
-            [Book.Name: "パーフェクトじゃないPHP", Book.Price: "3000", Book.PublishDate: "2013/04/13",
+            [Book.Name: "パーフェクトPHP", Book.Price: "3000", Book.PurchaseDate: "2013/04/13", Book.ImageUrl: "image1.png"],
+            [Book.Name: "CakePHP辞典", Book.Price: "3000", Book.PurchaseDate: "2013/04/13", Book.ImageUrl: "image2.png"],
+            [Book.Name: "パーフェクトじゃないPHP", Book.Price: "3000", Book.PurchaseDate: "2013/04/13",
              Book.ImageUrl: "image3.png"],
-            [Book.Name: "CakePHPじゃない辞典", Book.Price: "3000", Book.PublishDate: "2013/04/13",
+            [Book.Name: "CakePHPじゃない辞典", Book.Price: "3000", Book.PurchaseDate: "2013/04/13",
              Book.ImageUrl: "image4.png"]
         ]
     }
