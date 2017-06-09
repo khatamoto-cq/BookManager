341f078cb03a0522e22f60fa5f1de733d9428d89
hatamoto <k.hatamoto@caraquri.com>
2017-06-09 20:43:40 +0900

    設定画面のsegueの遷移をやめた

--- BookManager/SettingViewController.swift
+++ BookManager/SettingViewController.swift
@@ -4,7 +4,8 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var settingButton: UIButton!
 
     @IBAction func tapSettingAction(_ sender: Any) {
-        performSegue(withIdentifier: "setting", sender: self)
+        let controller = self.storyboard?.instantiateViewController(withIdentifier: "AccountNav")
+        present(controller!, animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
