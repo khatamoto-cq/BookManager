4749ae55c35d500dbc3cdeed1df239e5aaaaf8ef
hatamoto <k.hatamoto@caraquri.com>
2017-06-13 19:08:04 +0900

    ログイン後に保存するキーがuser_idでなかったので修正

--- BookManager/LoginViewController.swift
+++ BookManager/LoginViewController.swift
@@ -62,7 +62,7 @@ class LoginViewController: UIViewController {
 
     func saveAuthInfo(_ auth: Auth) {
         UserDefaults.standard.set(auth.requestToken, forKey: "request_token")
-        UserDefaults.standard.set(auth.userId, forKey: "login_id")
+        UserDefaults.standard.set(auth.userId, forKey: "user_id")
         UserDefaults.standard.synchronize()
     }
 
