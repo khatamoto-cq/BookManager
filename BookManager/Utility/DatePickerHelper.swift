6704227a070fbf2ab6f7dda43c47172fc7090b1c
hatamoto <k.hatamoto@caraquri.com>
2017-06-13 14:59:38 +0900

    Enumは省略形に修正

--- BookManager/Utility/DatePickerHelper.swift
+++ BookManager/Utility/DatePickerHelper.swift
@@ -4,7 +4,7 @@ import UIKit
 class DatePickerHelper {
     class func load(_ target: Any?, action: Selector, sender: UITextField) {
         let datePicker = UIDatePicker()
-        datePicker.datePickerMode = UIDatePickerMode.date
+        datePicker.datePickerMode = .date
         sender.inputView = datePicker
         datePicker.addTarget(target, action: action,
                              for: UIControlEvents.valueChanged)
