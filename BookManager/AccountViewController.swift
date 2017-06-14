6a058c26030344f854396d49b02a699324c990bf
hatamoto <k.hatamoto@caraquri.com>
2017-06-14 19:31:51 +0900

    アカウント設定にバリデーションを追加

--- BookManager/AccountViewController.swift
+++ BookManager/AccountViewController.swift
@@ -37,12 +37,21 @@ class AccountViewController: UIViewController {
     }
 
     @IBAction func didSaveButtonTapped(_ sender: Any) {
+        let errors = validate()
+        if errors.count > 0 {
+            return AlertHelper.showAlert(self, title: R.string.localizable.validateErrorTitle(),
+                                         message: errors.joined(separator: "\n"))
+        }
+
+        // (todo) 登録、更新処理
+
         if !UserDefaults.standard.bool(forKey: "logined") {
             saveLoginState()
-            let controller = R.storyboard.main.tabViewController()
-            controller?.modalTransitionStyle = .crossDissolve
-            present(controller!, animated: true, completion: nil)
         }
+
+        let controller = R.storyboard.main.tabViewController()
+        controller?.modalTransitionStyle = .crossDissolve
+        present(controller!, animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
@@ -52,5 +61,34 @@ class AccountViewController: UIViewController {
     func saveLoginState() {
         UserDefaults.standard.set(true, forKey: "logined")
         UserDefaults.standard.synchronize()
+    }
+
+    func validate() -> [String] {
+        var errors: [String] = []
+
+        if (emailTextField.text?.isEmpty)! {
+            errors.append(R.string.localizable.validateErrorRequireAccountEmail())
+        } else {
+            let regEx = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+            if !regEx.evaluate(with: emailTextField.text) {
+                errors.append(R.string.localizable.validateErrorInvalidEmail())
+            }
+        }
+
+        if (passwordTextField.text?.isEmpty)! {
+            errors.append(R.string.localizable.validateErrorRequireAccountPassword())
+        }
+
+        if (passwordConfirmTextField.text?.isEmpty)! {
+            errors.append(R.string.localizable.validateErrorRequireAccountPasswordConfirm())
+        }
+
+        if !(passwordTextField.text?.isEmpty)! && !(passwordConfirmTextField.text?.isEmpty)! {
+            if passwordTextField.text != passwordConfirmTextField.text {
+                errors.append(R.string.localizable.validateErrorCompareAccountPassword())
+            }
+        }
+
+        return errors
     }
 }
